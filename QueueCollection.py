d49917ba7e65dfdcfbf42cd4ce87760382daa1ff
JUAN CARLOS LOPERA ALVAREZ <juan.loperaa@udea.edu.co>
2022-04-20 13:59:17 -0500

    Creo clases de cola y pila

---
+++ QueueCollection.py
@@ -0,0 +1,15 @@
+class QueueCollection:
+    def __init__(self):
+        self.__data = []
+    
+    def QueueCollection():
+        pass
+    
+    def add(self, e):
+    
+    def remove(self, e):
+    
+    def find(self, e):
+        
+    def printQueue():
+
