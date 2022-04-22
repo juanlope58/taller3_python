0cb7d77f0a9bd7f326d8d56e9b0748524bf6723a
JUAN CARLOS LOPERA ALVAREZ <juan.loperaa@udea.edu.co>
2022-04-22 10:54:14 -0500

    Termino metodo add

--- QueueCollection.py
+++ QueueCollection.py
@@ -3,10 +3,26 @@ import Queue as qu
 class QueueCollection():
     def __init__(self):
         self.__data = qu.Queue()
-        self.temp=""
+        
     
     def add(self, e):
-        self.__data.enqueue(e) #se agrega elemento a la cola
+        if self.__data.isEmpty():
+            self.__data.enqueue(e) # si está vacío agrego a la cola
+        else:
+            self.__data.enqueue(e) # si no está vacío agrego a la cola
+            vec=[]
+            for i in range(self.__data.size()):
+                vec.append(self.__data.dequeue()) # saco la cola a una lista
+                # print(vec)
+            for j in range(len(vec)-1):           # ordeno de mayor a menor
+                for i in range(len(vec)-1):
+                    if (vec[i] < vec[i+1]):
+                        aux=vec[i]
+                        vec[i]=vec[i+1]
+                        vec[i+1]=aux
+            for i in range(len(vec)):
+                self.__data.enqueue(vec[i])            
+            
     
     def remove(self):
         return self.__data.dequeue()
@@ -24,4 +40,3 @@ class QueueCollection():
         # for i in self.__data.size:
         #     self.temp = self.__data.dequeue()
         #     print(self.temp)
-
